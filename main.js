d0c8f3ae426c34a859c53348e9b4faff13bc8817
moclan110 <vulan11101997@gmail.com>
2022-11-11 08:16:46 +0700

    may tinh don gian

---
+++ main.js
@@ -0,0 +1,15 @@
+// let result = '';
+// function xuLyduLieu(button) {
+//     let a = document.getElementById(button).value;
+//     result += a;
+//     document.getElementById('result').innerHTML = result;
+// }
+// function xuLyketQua() {
+//     let ketQua = eval(result);
+//     document.getElementById('result').innerHTML = ketQua;
+//     result = ketQua;
+// }
+// function xoa() {
+//     document.getElementById('result').innerHTML = '0';
+//     result = '';
+// }
